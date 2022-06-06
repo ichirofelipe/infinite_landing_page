5b3cf2b9ce9eec05e5a6c934cc7eccc69aea708f
ichirofelipe <ichiro.felipe1696@gmail.com>
2022-06-06 17:49:58 +0800

    added updated at in table column list

--- action/websites_list.php
+++ action/websites_list.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $limit = $toShow??9;
     $pages = $pageDisplay??5;
     $skip = $limit * ($listPage - 1);
-    $websites = selectQuery('websites', $skip, $limit, 'websites_id,websites_domain,websites_title,websites_description,websites_created_at');
+    $websites = selectQuery('websites', $skip, $limit, 'websites_id,websites_domain,websites_title,websites_description,websites_created_at,websites_updated_at');
     $columns = getColumns($websites[0]??$websites);
     $totalWebsites = countQuery('websites');
     $pagination = paginate($totalWebsites['count'], $listPage, $limit, $pages);
